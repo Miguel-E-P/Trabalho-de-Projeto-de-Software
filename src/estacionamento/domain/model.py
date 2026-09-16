663d24f7e8a7cdcac0656f153b7c362b563247a5
kndysoares <85966832+kndysoares@users.noreply.github.com>
2026-09-16 10:58:13 -0300

    Update model.py "estrutura inicial das classes do agregado cliente"
    Foi escrito a estrutura inicial das classes do agregado cliente/ veiculo, indicando qual é value object, entidade e entidade raiz. Com a ordenação da class mais simples até chegar na class raiz cliente, as dependências são lidas a classe de baixo depende da classe de cima.

--- src/estacionamento/domain/model.py
+++ src/estacionamento/domain/model.py
@@ -1,1 +1,32 @@
+''' Peço aos colegas que concentrem as importações de lib aqui em cima,
+na medida do possível'''
 
+from dataclasses import dataclass
+
+
+# Agregado Cliente / Veículo
+# Abordagem: da class mais simples até chegar na class raiz
+
+#class value object 
+@dataclass(frozen=True)
+class Placa:
+    numero = str
+
+#class entidade
+class Veiculo:
+    id_veiculo = int
+    '''atributo placa depende do uso de uma placa da class de cima (Placa)'''
+    placa = str 
+    tipo = str
+
+#class entidade raiz do agregado
+class Cliente:
+    id_cliente = int
+    nome = str
+    '''atributo veiculo depende de um veiculo da class de cima (Veiculo)'''
+    veiculo = str 
+#método que vai cadastrar veiculo para um cliente
+    def cadastrar_veiculo():
+        placas = str
+#Fim do agregado cliente/ veiculo
+      
