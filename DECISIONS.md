fc4e2a0f9315896bc64154e0856f2197792223ca
Gustavo Teixeira <gustavot@id.uff.br>
2026-09-16 22:42:35 -0300

    refact: Formatando o código e adicionando minha entrada no Decisions.md

--- DECISIONS.md
+++ DECISIONS.md
@@ -1,6 +1,17 @@
+# Plataforma de Controle e Gestão de Estacionamentos
+## Project Backlog (nossas decisões)
+
+### Kennedy
+**16-09-26**:
 Optei por realizar a estrutura inicial das classes do agregado cliente/ veiculo para deixar explicitado as dependências de classe de cima pra baixo,
 sinalizando por comentários qual atributo específico depende da classe de cima para sua composição. Cada classe já está com sua classificação:
 value object, entidade simples e entidade raiz. A class Placa não possui atributo "id" própria por que foi classificada como value object, pois,
 é definida apenas pelo valor e é imutável.
 
+### Pedro
+**16-09-26**:
 Pedro: Para proteger contra golpes. Vamos precisar de algum sistema de verificacao de tempo. Ou seja, quando o pagamento ocorrer, e o ticket for atualizado para valido, o sistema precisa verificar se o ticket foi usado ate certo periodo de tempo, se nao, atualizar ele para invalido novamente. Pois se nao, teremos um sistema em que um cliente pode entrar no estacionamento, pagar o ticket, e permanecer horas/dias a mais no estacionamento, e sair no fim de tudo tendo pago como se tivesse ficado apenas minutos dentro do estacionamento.
+
+### Gustavo
+**16-09-26**:
+Primeiro, estou organizando nosso repositório; devo fazer esse tipo de varredura mais vezes e, a não ser que eu esqueça, subir o commit com uma flag de "_refact_". A título de curiosidade, o linter/code formatter que uso chama-se [ruff](https://docs.astral.sh/ruff/).
--- src/estacionamento/domain/model.py
+++ src/estacionamento/domain/model.py
@@ -1,70 +1,70 @@
-''' Peço aos colegas que concentrem as importações de lib aqui em cima,
-na medida do possível'''
+"""Peço aos colegas que concentrem as importações de lib aqui em cima,
+na medida do possível"""
 
 from dataclasses import dataclass
-
 
 # Agregado Cliente / Veículo
 # Abordagem: da class mais simples até chegar na class raiz
 
-#class value object 
+
+# class value object
 @dataclass(frozen=True)
 class Placa:
     numero = str
 
-#class entidade
+
+# class entidade
 class Veiculo:
     id_veiculo = int
-    '''atributo placa depende do uso de uma placa da class de cima (Placa)'''
-    placa = str 
+    """atributo placa depende do uso de uma placa da class de cima (Placa)"""
+    placa = str
     tipo = str
 
-#class entidade raiz do agregado
+
+# class entidade raiz do agregado
 class Cliente:
     id_cliente = int
     nome = str
-    '''atributo veiculo depende de um veiculo da class de cima (Veiculo)'''
-    veiculo = str 
-#método que vai cadastrar veiculo para um cliente
+    """atributo veiculo depende de um veiculo da class de cima (Veiculo)"""
+    veiculo = str
+
+    # método que vai cadastrar veiculo para um cliente
     def cadastrar_veiculo():
         placas = str
-#Fim do agregado cliente/ veiculo
 
-#agregado pagamento
 
-#talvez seja necessario add uma forma de guardar o tipo de pagamento
-#isso seria vantajoso para o sistema ou n?
+# Fim do agregado cliente/ veiculo
 
-#ini-dinheiro
+# agregado pagamento
+
+# talvez seja necessario add uma forma de guardar o tipo de pagamento
+# isso seria vantajoso para o sistema ou n?
+
+
+# ini-dinheiro
 @dataclass
 class Dinheiro:
     valor: float
-    
-    #protege contra valor negativo. Seria isso um Invariante?
+
+    # protege contra valor negativo. Seria isso um Invariante?
     def __post_init__(self):
         if self.valor < 0.0:
             raise ValueError("Valor NAO pode ser negativo.")
-#fim-dinheiro
 
-#ini-pagamento
+
+# fim-dinheiro
+
+
+# ini-pagamento
 @dataclass
 class Pagamento:
-    id_pagamento: int #int do python eh bizarro. Esse kra cresce em bytes qnd necessario?!?!?!??! Bruxaria
+    id_pagamento: int  # int do python eh bizarro. Esse kra cresce em bytes qnd necessario?!?!?!??! Bruxaria
     id_ticket: int
     valor: Dinheiro
     pago: bool = False
 
     def pagar(self):
         self.pago = True
-#fim-pagamento
 
 
-
-
-
-
-
-
-
-
-
+# fim-pagamento
