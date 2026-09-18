dab4afa3e41b9686bc7a2e6ba6f9f67abeb79b8a
sigurd-the-red <quintaes_pedro@id.uff.br>
2026-09-18 15:08:57 -0700

    Adicionei testes para inicializacao e execucao de Pagamento

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -44,4 +44,27 @@ def test_dinheiro_imutabilidade():
     dinheiro = Dinheiro(15.0)
     with pytest.raises(FrozenInstanceError):
         dinheiro.valor = 25.0
+
+def test_pagamento_inicializacao():
+    valor = Dinheiro(valor=50.0)
+    pagamento = Pagamento(id_pagamento=1, id_ticket=100, valor=valor)
+
+    assert pagamento.id_pagamento == 1
+    assert pagamento.id_ticket == 100
+    assert pagamento.valor == valor
+    assert pagamento.pago is False
+    assert pagamento.tipo_pagamento is None
+    assert pagamento.data_hora_pagamento is None
+
+def test_pagamento_execucao_sucesso():
+    valor = Dinheiro(valor=35.0)
+    pagamento = Pagamento(id_pagamento=1, id_ticket=100, valor=valor)
+    momento_pagamento = datetime(2026, 9, 18, 10, 30, 0)
+
+    pagamento.pagar(tipo=TipoPagamento.PIX, momento=momento_pagamento)
+
+    assert pagamento.pago is True
+    assert pagamento.tipo_pagamento == TipoPagamento.PIX
+    assert pagamento.data_hora_pagamento == momento_pagamento
+
 #FIM AGREGADO PAGAMENTO
