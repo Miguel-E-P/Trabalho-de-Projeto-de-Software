37c764a95e362912fd253fe1270d6a87e3bec3ca
sigurd-the-red <quintaes_pedro@id.uff.br>
2026-09-18 06:33:39 -0700

    Adicionei testes para Dinheiro e ajeitei o import

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,10 +1,13 @@
+from uuid import uuid4
+from dataclasses import FrozenInstanceError
+
+import pytest
+
+import src.estacionamento.domain.model
+
+
 #Testes unitários para o agregado cliente
 #Primeiro teste: verificar se um cliente consegue cadastrar um veículo
-
-from uuid import uuid4
-
-import estacionamento.domain.model
-
 
 def test_cliente_pode_cadastrar_um_veiculo():
     cliente = estacionamento.domain.model.Cliente(
@@ -26,3 +29,19 @@ def test_cliente_pode_cadastrar_um_veiculo():
     assert veiculo in cliente.veiculos
     
 #Fim do primeiro teste no agregado cliente
+
+
+#INI AGREGADO PAGAMENTO
+def test_dinheiro_invariante_negativa():
+    with pytest.raises(ValueError, match="Valor NAO pode ser negativo."):
+        Dinheiro(valor=-67.00)
+
+def test_dinheiro_igualdade():
+    assert Dinheiro(67.0) == Dinheiro(67.0)
+    assert Dinheiro(67.0) != Dinheiro(14.0)
+
+def test_dinheiro_imutabilidade():
+    dinheiro = Dinheiro(15.0)
+    with pytest.raises(FrozenInstanceError):
+        dinheiro.valor = 25.0
+#FIM AGREGADO PAGAMENTO
