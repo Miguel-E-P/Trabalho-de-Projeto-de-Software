678889036d73b21cedd27c6c4f00e8b022d11e12
Henrique <mhreis2407@gmail.com>
2026-09-20 22:54:20 -0300

    Update test_services.py

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -14,19 +14,4 @@ def test_calculadora_tolerancia():
 
 #Fim do primeiro teste no Domain Service CalculadoraTarifa
 
-# #Segundo teste: Tentativa de reserva conflitante
-# def test_verificador_bloqueia_conflito_de_horario():
-#     vaga = Vaga(id_vaga=1, tipo=TipoVaga.CARRO, status=StatusVaga.LIVRE)
-#     reserva_existente = Reserva(
-#         id_reserva=101,
-#         data_reserva=datetime(2026, 9, 20, 14, 0, 0),
-#         data_fim=datetime(2026, 9, 20, 16, 0, 0),
-#         placa_veiculo_reserva="ABC1D23",
-#     )
 
-#     inicio_tentativa = datetime(2026, 9, 20, 15, 0, 0)
-#     fim_tentativa = datetime(2026, 9, 20, 17, 0, 0)
-
-#     assert (VerificarDisponibilidadeVaga.validar(vaga, inicio_tentativa, fim_tentativa, [reserva_existente]) is False)
-
-# #Fim do segundo teste no Domain Service VerificadorDisponibilidadeVaga
